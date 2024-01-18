b8c2e29ceef87668e4bbb319472325fc93b96e1b
awsdevopsonline <punnam.elite@gmail.com>
2024-01-18 13:29:17 +0530

    TEST

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,10 @@
+output "azs" {
+  value = data.aws_availability_zones.azs.names
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
-output "azs" {
-    value = data.aws_availability_zones.azs.names
-}
-
 
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
